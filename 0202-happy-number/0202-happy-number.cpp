2ca00e34fcb6c16a926cf8126c35c85a83d98fdf
Lokesh Kumar <lokeshkr.tech@gmail.com>
2026-08-04 00:10:30 +0530

    Time: 0 ms (100%), Space: 7.6 MB (97.51%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,21 +1,24 @@
 class Solution {
-private:
-    int nextNumber(int n) {
-        int newNumber = 0 ;
-        while(n!=0){
-            int num = n %10;
-            newNumber += num*num;
-            n= n/10;
-        }
-        return newNumber;
-    }
 public:
-    bool isHappy(int n) {
-        unordered_set<int> set;;
-        while (n!=1 && !set.count(n)){
-            set.insert(n);
-            n = nextNumber(n);
+    int findSquare(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
         }
-        return n==1;
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = findSquare(n);
+
+        while (slow != fast) {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        }
+
+        return slow == 1;
     }
 };
